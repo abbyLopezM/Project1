7c517068bec42dd6cea5d84398b4bd603f096d5d
ALM <abbychaparra16@gmail.com>
2022-10-29 23:07:46 -0700

    add feature for radio buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,29 @@ var questions = [
         //use keyWordQuery
     question: 'Select all genres you currently listen to.',
     options: ['Rock','Jazz','Electro','Techno','Metal','Country', 'Pop', 'Blues'],
+    type: 'queryadd',
     },
     {
         //use keyWordQuery
         question: 'Do you prefer instrumental or vocals?',
         options: ['Instrumental','Vocals'],
+        type: 'queryadd',
     },
     {
         //use license
         question: 'Do you prefer a channel recommended by youtube or Creative Common?',
         options: ['Youtube','Creative Common'],
+        type: 'license',
     },
     { //use videoDuration
         question: 'How long of a video would you like to watch?',
         options: ['< 4 Min','> 4 Min','> 20 Min'],
+        type: 'length',
     },
     {// use removeQuery
         question: 'What do you not want to watch?',
         options: ['covers','blank','blank'],
+        type: 'queryremove',
     }
 ]
 var startBtn = document.querySelector("#startBtn");
@@ -28,10 +33,11 @@ var prompts = document.querySelector("#question-container");
 var intro = document.querySelector("#intro-page");
 var questionsEL= document.querySelector('#question-container');
 
+
 var currentQuestionIndex = 0;
 // begins to show the questions
 //query words to add
-var keyWordQuery;
+const keyWordQuery=[];
 //query words to remove
 var removeQuery;
 //youtube or creativeCommon
@@ -60,33 +66,77 @@ function showQuestions() {
         divQ.textContent = currentQuestion.question;
         console.log(divQ);
         questionsEL.append(divQ);
-        for(var i = 0; i<currentQuestion.options.length; i++){
 
-            console.log('in loop');
-            var label = document.createElement('label');
-            label.htmlFor = currentQuestion.options[i];
-            var checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            checkbox.name = currentQuestion.options[i];
-            checkbox.value = currentQuestion.options[i];
-            checkbox.id = currentQuestion.options[i];
-            var span = document.createElement('span');
-            span.textContent = currentQuestion.options[i];
-            questionsEL.appendChild(label);
-            label.appendChild(checkbox);
-            label.appendChild(span);
-            divQ.appendChild(label);
+        if(index>0 && index<4){
+            for(var i = 0; i<currentQuestion.options.length; i++){
 
-            checkbox.style.display="block";
-            label.style.display="block";
+                console.log('we got in');
+                var label = document.createElement('label');
+                label.htmlFor = currentQuestion.options[i];
+                var radio = document.createElement('input');
+                radio.type = "radio";
+                radio.class = 'radio';
+                radio.name = currentQuestion.options[i];
+                radio.value = currentQuestion.options[i];
+                radio.id = currentQuestion.options[i];
+                var span = document.createElement('span');
+                span.textContent = currentQuestion.options[i];
+                questionsEL.appendChild(label);
+                label.appendChild(radio);
+                label.appendChild(span);
+                divQ.appendChild(label);
+                radio.style.display="block";
+                label.style.display="block";
+
+            }
+            console.log('outofloop');
+            currentQuestionIndex++;
         }
-        console.log('outofloop');
-        currentQuestionIndex++;
+        else{
+            for(var i = 0; i<currentQuestion.options.length; i++){
+
+                console.log('in loop');
+                var label = document.createElement('label');
+                label.htmlFor = currentQuestion.options[i];
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.name = currentQuestion.options[i];
+                checkbox.value = currentQuestion.options[i];
+                checkbox.id = currentQuestion.options[i];
+                var span = document.createElement('span');
+                span.textContent = currentQuestion.options[i];
+                questionsEL.appendChild(label);
+                label.appendChild(checkbox);
+                label.appendChild(span);
+                divQ.appendChild(label);
+                checkbox.style.display="block";
+                label.style.display="block";
+                //checkbox.addEventListener('click', checkAnswer);
+
+
+            }
+            console.log('outofloop');
+            currentQuestionIndex++;
+        }
+
     }
-        
 }
 
+// function checkAnswer(event){
+//     console.log(questions[currentQuestionIndex].options)
+//     console.log('in check anwer')
+//     if(questions[currentQuestionIndex].options == event.target.innerText){
+//         keyWordQuery.push(questions[currentQuestionIndex].options);
+//         console.log(keyWordQuery);
+//     }
+//     else{
+//         console.log('wrong');
+//     }
+//     currentQuestionIndex++;
+//     showQuestions();
+// }
 startBtn.addEventListener('click', showQuestions);
+//checkbox.addEventListener('click', checkAnswer);
 
 // put into function that will show results page and hide questions
 //resultElement.style.display = "block";
